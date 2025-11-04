eb516a2c6a5507852c2a12532926a377b0da296a
André <145787269+Andreheinzen@users.noreply.github.com>
2025-11-04 14:25:01 -0300

    04/11

--- main.js
+++ main.js
@@ -11,11 +11,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Demonstra entusiasmo para conhecer gente nova",
-                afirmacao: " Você é o tipo de pessoa que se sente energizada em ambientes sociais. Para você, a interação com os outros é a principal fonte de energia, e você adora a oportunidade de conversar e criar novas conexões. ",
+                afirmacao: [
+                    "Você é o tipo de pessoa que se sente energizada em ambientes sociais. Para você, a interação com os outros é a principal fonte de energia, e você adora a oportunidade de conversar e criar novas conexões. ",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             },
             {
                 texto: " Prefere eventos mais calmos e selecionados. ",
-                afirmacao: " Você prefere encontros íntimos e conversas profundas, recarregando suas energias no tempo a sós ou com poucas pessoas de confiança.",
+                afirmacao: [
+                    " Você prefere encontros íntimos e conversas profundas, recarregando suas energias no tempo a sós ou com poucas pessoas de confiança.",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             }
         ]
     },
@@ -24,11 +32,20 @@ const perguntas = [
         alternativas: [
             {
                 texto: " Revigorado, pronto para sair de novo! ",
-                afirmacao: " Você se recarrega com a interação social e se sente mais produtiva e inspirada em ambientes de grupo, onde a troca de ideias estimula sua criatividade e colaboração. ",
+                afirmacao: [
+                    "Você se recarrega com a interação social e se sente mais produtiva e inspirada em ambientes de grupo, onde a troca de ideias estimula sua criatividade e colaboração. ",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             },
+
             {
                 texto: " Precisa de um tempo a sós para relaxar  ",
-                afirmacao: " Você precisa de tempo sozinho para se reconectar, pois o excesso de estímulo social pode te cansar. Por isso, valoriza momentos de paz para renovar suas energias e organizar suas ideias. ",
+                afirmacao: [
+                    " Você precisa de tempo sozinho para se reconectar, pois o excesso de estímulo social pode te cansar. Por isso, valoriza momentos de paz para renovar suas energias e organizar suas ideias. ",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             }
         ]
     },
@@ -36,12 +53,20 @@ const perguntas = [
         enunciado: " Qual o seu estilo em uma conversa em grupo?",
         alternativas: [
             {
-                texto: "   Gosta de contar histórias e participar ativamente ",
-                afirmacao: "Você tem uma habilidade natural de se comunicar, fazendo qualquer conversa fluir e se tornando a alma do ambiente.",
+                texto: "Gosta de contar histórias e participar ativamente ",
+                afirmacao: [
+                    "Você tem uma habilidade natural de se comunicar, fazendo qualquer conversa fluir e se tornando a alma do ambiente.",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             },
             {
                 texto: " Prefere ouvir mais do que falar e contribuir apenas quando necessário ",
-                afirmacao: "Você é um ouvinte atento e tranquilo, sempre absorvendo as informações com calma. Suas contribuições são sempre precisas e vêm com propósito, adicionando valor à conversa.",
+                afirmacao: [
+                    "Você é um ouvinte atento e tranquilo, sempre absorvendo as informações com calma. Suas contribuições são sempre precisas e vêm com propósito, adicionando valor à conversa.",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             }
         ]
     },
@@ -49,12 +74,20 @@ const perguntas = [
         enunciado: "No fim de semana ideal, o que não pode faltar?",
         alternativas: [
             {
-                texto: " Eventos com amigos e família",
-                afirmacao: " Para você, o fim de semana perfeito é cheio de movimento e socialização. Sua agenda fica repleta de eventos, sempre em busca de novas interações e experiências.   ",
+                texto: "Eventos com amigos e família",
+                afirmacao: [
+                    "Para você, o fim de semana perfeito é cheio de movimento e socialização. Sua agenda fica repleta de eventos, sempre em busca de novas interações e experiências.",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             },
             {
                 texto: " Tempo para ler, ver um filme ou simplesmente relaxar em casa. ",
-                afirmacao: " Para você, o fim de semana é um verdadeiro refúgio, onde a paz e o tempo sozinho são essenciais para recarregar as energias e encontrar equilíbrio. ",
+                afirmacao: [
+                    "Para você, o fim de semana é um verdadeiro refúgio, onde a paz e o tempo sozinho são essenciais para recarregar as energias e encontrar equilíbrio. ",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             }
         ]
     },
@@ -63,11 +96,19 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Conversa com entusiasmo e compartilha detalhes da vida",
-                afirmacao: "Você se abre com facilidade e, com sua habilidade de se expressar, cria novas conexões de forma natural e fluida.",
+                afirmacao: [
+                    "Você se abre com facilidade e, com sua habilidade de se expressar, cria novas conexões de forma natural e fluida.",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             },
             {
                 texto: " Observa a pessoa e só se abre quando sente confiança ",
-                afirmacao: " Você tende a ser mais cauteloso preferindo que as conexões se desenvolvam de maneira gradual, e só se abre plenamente com pessoas em quem realmente confia. ",
+                afirmacao: [
+                    "Você tende a ser mais cauteloso preferindo que as conexões se desenvolvam de maneira gradual, e só se abre plenamente com pessoas em quem realmente confia. ",
+                    "afirmaçao 2",
+                    "afirmaçao 3"
+                ]
             }
         ]
     },
@@ -77,31 +118,31 @@ let atual = 0;
 let perguntaAtual;
 let historiaFinal
 
-function mostraPergunta(){
- if(atual>= perguntas.length){
-   mostraResultado()
-   return;
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado()
+        return;
 
- }
- 
- 
+    }
+
+
     perguntaAtual = perguntas[atual];
-  caixaPerguntas.textContent = perguntaAtual.enunciado
-  caixaAlternativas.textContent = "  ";
-  mostraAlternativas();
+    caixaPerguntas.textContent = perguntaAtual.enunciado
+    caixaAlternativas.textContent = "  ";
+    mostraAlternativas();
 }
 
-function mostraAlternativas(){
+function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto
-        botaoAlternativas.addEventListener('click',  () => respostaSelecionada(alternativa));
-       caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.addEventListener('click', () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
 
-function respostaSelecionada(opcaoSelecionada){
+function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++
@@ -109,7 +150,7 @@ function respostaSelecionada(opcaoSelecionada){
 
 }
 
-function mostraResultado(){
+function mostraResultado() {
     caixaPerguntas.textContent = "Se fosse possível...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "  ";
